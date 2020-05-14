8f3e45433963dbb59fc7e3b3c3646045c80048ff
drakeerv <60192521+drakeerv@users.noreply.github.com>
2020-05-14 13:19:32 -0400

    Add files via upload

---
+++ CameraChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//script made by drakeerv
+
+public class CameraChange : MonoBehaviour
+{
+    public Camera FirstPerson;
+    public Camera ThirdPerson;
+    public int StartingCam;
+    private int CurrentCamera;
+
+    void Start()
+    {
+        if (StartingCam == 1)
+        {
+            ThirdPerson.enabled = false;
+            FirstPerson.enabled = true;
+        }
+        if (StartingCam == 2)
+        {
+            ThirdPerson.enabled = true;
+            FirstPerson.enabled = false;
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            ThirdPerson.enabled = true;
+            FirstPerson.enabled = false;
+            print("switching to third person");
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            ThirdPerson.enabled = false;
+            FirstPerson.enabled = true;
+            print("switching to first person");
+        }
+    }
+}
